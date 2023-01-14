c1e5782959ae3d47754c0467e00648a41bd34423
Sergiu <sergey.roxigo@gmail.com>
2023-01-14 10:03:01 -0800

    ...

--- application/controllers/figma.php
+++ application/controllers/figma.php
@@ -1,19 +1,16 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class JSON2Figma extends CI_Controller {
+class Figma extends CI_Controller {
 
 	public function __construct()
 	{
 		/* Call CodeIgniter's default Constructor */
 		parent::__construct();
-
-		// figd_J7gcXXIc-T5g9SWYcu3gIH5MpGO8O5r_NFHLCzIl
 	}
 
-	public function parse()
+	public function import()
 	{
-
-		echo 'Final!';
+		echo 'Hi@';
 	}
 }
